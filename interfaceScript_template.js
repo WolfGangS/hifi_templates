98be4fccc49697af0dfa41303755be1992ec1d7a
WolfGang <flamin2k8@gmail.com>
2018-04-09 17:11:21 +0100

    Update interfaceScript_template.js

--- interfaceScript_template.js
+++ interfaceScript_template.js
@@ -1,13 +1,12 @@
 
-
-/* globals Script */
-
-
+//Define out scope
 (function() {
 
-  
+  //Hook the script ending event
   Script.scriptEnding.connect(scriptEnding);
   
+  //Method called when script is ending, cleanup needs to happen here
+  //Any overlays, events you hanve connected to etc, need to be removed/disconnected
   function scriptEnding(){
     
   }
